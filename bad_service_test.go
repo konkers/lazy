a1162a3d5a6d34eda9e6bd2ae48a6cf426a59a33
Erik Gilling <konkers@google.com>
2018-10-23 22:58:04 -0700

    Add Context support.

--- bad_service_test.go
+++ bad_service_test.go
@@ -1,6 +1,9 @@
 package lazy
 
-import "testing"
+import (
+	"context"
+	"testing"
+)
 
 type privateData struct{}
 
@@ -18,25 +21,32 @@ type TestServiceBadGetInNum struct {
 func (s *TestServiceBadGetInNum) Get() {
 }
 
+type TestServiceBadGetIn0 struct {
+	TestService
+}
+
+func (s *TestServiceBadGetIn0) Get(ctx string, id int) {
+}
+
 type TestServiceBadGetIn1 struct {
 	TestService
 }
 
-func (s *TestServiceBadGetIn1) Get(id string) {
+func (s *TestServiceBadGetIn1) Get(ctx context.Context, id string) {
 }
 
 type TestServiceBadGetOutNum struct {
 	TestService
 }
 
-func (s *TestServiceBadGetOutNum) Get(id int) {
+func (s *TestServiceBadGetOutNum) Get(ctx context.Context, id int) {
 }
 
 type TestServiceBadGetOut0 struct {
 	TestService
 }
 
-func (s *TestServiceBadGetOut0) Get(id int) (int, error) {
+func (s *TestServiceBadGetOut0) Get(ctx context.Context, id int) (int, error) {
 	return 0, nil
 }
 
@@ -44,7 +54,7 @@ type TestServiceBadGetOut0Private struct {
 	TestService
 }
 
-func (s *TestServiceBadGetOut0Private) Get(id int) (*privateData, error) {
+func (s *TestServiceBadGetOut0Private) Get(ctx context.Context, id int) (*privateData, error) {
 	return nil, nil
 }
 
@@ -52,7 +62,7 @@ type TestServiceBadGetOut1 struct {
 	TestService
 }
 
-func (s *TestServiceBadGetOut1) Get(id int) (*TestData, int) {
+func (s *TestServiceBadGetOut1) Get(ctx context.Context, id int) (*TestData, int) {
 	return nil, 0
 }
 
@@ -63,7 +73,7 @@ func (s *TestServiceBadGetOut1) Get(id int) (*TestData, int) {
 type TestServiceNoPut struct {
 }
 
-func (s *TestServiceNoPut) Get(id int) (*TestData, error) {
+func (s *TestServiceNoPut) Get(ctx context.Context, id int) (*TestData, error) {
 	return nil, nil
 }
 
@@ -78,28 +88,35 @@ type TestServiceBadPutIn0 struct {
 	TestService
 }
 
-func (s *TestServiceBadPutIn0) Put(id string, data *TestData) {
+func (s *TestServiceBadPutIn0) Put(ctx string, id int, data *TestData) {
 }
 
 type TestServiceBadPutIn1 struct {
 	TestService
 }
 
-func (s *TestServiceBadPutIn1) Put(id int, data int) {
+func (s *TestServiceBadPutIn1) Put(ctx context.Context, id string, data *TestData) {
+}
+
+type TestServiceBadPutIn2 struct {
+	TestService
+}
+
+func (s *TestServiceBadPutIn2) Put(ctx context.Context, id int, data int) {
 }
 
 type TestServiceBadPutOutNum struct {
 	TestService
 }
 
-func (s *TestServiceBadPutOutNum) Put(id int, data *TestData) {
+func (s *TestServiceBadPutOutNum) Put(ctx context.Context, id int, data *TestData) {
 }
 
 type TestServiceBadPutOut0 struct {
 	TestService
 }
 
-func (s *TestServiceBadPutOut0) Put(id int, data *TestData) int {
+func (s *TestServiceBadPutOut0) Put(ctx context.Context, id int, data *TestData) int {
 	return 0
 }
 
@@ -110,11 +127,11 @@ func (s *TestServiceBadPutOut0) Put(id int, data *TestData) int {
 type TestServiceNoNew struct {
 }
 
-func (s *TestServiceNoNew) Get(id int) (*TestData, error) {
+func (s *TestServiceNoNew) Get(ctx context.Context, id int) (*TestData, error) {
 	return nil, nil
 }
 
-func (s *TestServiceNoNew) Put(id int, data *TestData) error {
+func (s *TestServiceNoNew) Put(ctx context.Context, id int, data *TestData) error {
 	return nil
 }
 
@@ -129,21 +146,28 @@ type TestServiceBadNewIn0 struct {
 	TestService
 }
 
-func (s *TestServiceBadNewIn0) New(data int) {
+func (s *TestServiceBadNewIn0) New(ctx int, data *TestData) {
+}
+
+type TestServiceBadNewIn1 struct {
+	TestService
+}
+
+func (s *TestServiceBadNewIn1) New(ctx context.Context, data int) {
 }
 
 type TestServiceBadNewOutNum struct {
 	TestService
 }
 
-func (s *TestServiceBadNewOutNum) New(data *TestData) {
+func (s *TestServiceBadNewOutNum) New(ctx context.Context, data *TestData) {
 }
 
 type TestServiceBadNewOut0 struct {
 	TestService
 }
 
-func (s *TestServiceBadNewOut0) New(data *TestData) (string, error) {
+func (s *TestServiceBadNewOut0) New(ctx context.Context, data *TestData) (string, error) {
 	return "", nil
 }
 
@@ -151,7 +175,7 @@ type TestServiceBadNewOut1 struct {
 	TestService
 }
 
-func (s *TestServiceBadNewOut1) New(data *TestData) (int, string) {
+func (s *TestServiceBadNewOut1) New(ctx context.Context, data *TestData) (int, string) {
 	return 0, ""
 }
 
@@ -162,15 +186,15 @@ func (s *TestServiceBadNewOut1) New(data *TestData) (int, string) {
 type TestServiceNoDelete struct {
 }
 
-func (s *TestServiceNoDelete) Get(id int) (*TestData, error) {
+func (s *TestServiceNoDelete) Get(ctx context.Context, id int) (*TestData, error) {
 	return nil, nil
 }
 
-func (s *TestServiceNoDelete) Put(id int, data *TestData) error {
+func (s *TestServiceNoDelete) Put(ctx context.Context, id int, data *TestData) error {
 	return nil
 }
 
-func (s *TestServiceNoDelete) New(data *TestData) (int, error) {
+func (s *TestServiceNoDelete) New(ctx context.Context, data *TestData) (int, error) {
 	return 0, nil
 }
 
@@ -185,21 +209,28 @@ type TestServiceBadDeleteIn0 struct {
 	TestService
 }
 
-func (s *TestServiceBadDeleteIn0) Delete(id string) {
+func (s *TestServiceBadDeleteIn0) Delete(ctx int, id int) {
+}
+
+type TestServiceBadDeleteIn1 struct {
+	TestService
+}
+
+func (s *TestServiceBadDeleteIn1) Delete(ctx context.Context, id string) {
 }
 
 type TestServiceBadDeleteOutNum struct {
 	TestService
 }
 
-func (s *TestServiceBadDeleteOutNum) Delete(id int) {
+func (s *TestServiceBadDeleteOutNum) Delete(ctx context.Context, id int) {
 }
 
 type TestServiceBadDeleteOut0 struct {
 	TestService
 }
 
-func (s *TestServiceBadDeleteOut0) Delete(id int) string {
+func (s *TestServiceBadDeleteOut0) Delete(ctx context.Context, id int) string {
 	return ""
 }
 
@@ -214,7 +245,8 @@ func TestBadService(t *testing.T) {
 		// Get
 		{&TestServiceNoGet{}, "Expected error from service without Get"},
 		{&TestServiceBadGetInNum{}, "Expected error from bad Get in arg count"},
-		{&TestServiceBadGetIn1{}, "Expected error from bad Get argument type"},
+		{&TestServiceBadGetIn0{}, "Expected error from bad Get argument 0 type"},
+		{&TestServiceBadGetIn1{}, "Expected error from bad Get argument 1 type"},
 		{&TestServiceBadGetOutNum{}, "Expected error from bad Get out arg count"},
 		{&TestServiceBadGetOut0{}, "Expected error from bad Get out arg 0"},
 		{&TestServiceBadGetOut0Private{}, "Expected error from private Get out arg 0"},
@@ -225,6 +257,7 @@ func TestBadService(t *testing.T) {
 		{&TestServiceBadPutInNum{}, "Expected error from bad Put in arg count"},
 		{&TestServiceBadPutIn0{}, "Expected error from bad Put argument 0 type"},
 		{&TestServiceBadPutIn1{}, "Expected error from bad Put argument 1 type"},
+		{&TestServiceBadPutIn2{}, "Expected error from bad Put argument 2 type"},
 		{&TestServiceBadPutOutNum{}, "Expected error from bad Put out arg count"},
 		{&TestServiceBadPutOut0{}, "Expected error from bad Put out arg 0"},
 
@@ -232,6 +265,7 @@ func TestBadService(t *testing.T) {
 		{&TestServiceNoNew{}, "Expected error from service without New"},
 		{&TestServiceBadNewInNum{}, "Expected error from bad New in arg count"},
 		{&TestServiceBadNewIn0{}, "Expected error from bad New argument 0 type"},
+		{&TestServiceBadNewIn1{}, "Expected error from bad New argument 1 type"},
 		{&TestServiceBadNewOutNum{}, "Expected error from bad New out arg count"},
 		{&TestServiceBadNewOut0{}, "Expected error from bad New out arg 0"},
 		{&TestServiceBadNewOut1{}, "Expected error from bad New out arg 1"},
@@ -240,6 +274,7 @@ func TestBadService(t *testing.T) {
 		{&TestServiceNoDelete{}, "Expected error from service without Delete"},
 		{&TestServiceBadDeleteInNum{}, "Expected error from bad Delete in arg count"},
 		{&TestServiceBadDeleteIn0{}, "Expected error from bad Delete argument 0 type"},
+		{&TestServiceBadDeleteIn1{}, "Expected error from bad Delete argument 1 type"},
 		{&TestServiceBadDeleteOutNum{}, "Expected error from bad New out arg count"},
 		{&TestServiceBadDeleteOut0{}, "Expected error from bad Delete out arg 0"},
 	}
